08db3305a4cff1415aaa7a8f0269f2449e81bcc0
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-11-29 17:00:18 +0530

    Add Services for  login functionality

---
+++ BSEBApp/src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+   apiUrl = 'https://localhost:7120/api/loginauth/login';
+    // apiUrl = 'https://your-api-url.com/api/login';
+  constructor(private http:HttpClient ) { }
+
+  // login(credentials: { username: string; password: string }): Observable<any> {
+  //   return this.http.post<any>(this.apiUrl, credentials);
+  // }
+  login(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data);
+  }
+}
