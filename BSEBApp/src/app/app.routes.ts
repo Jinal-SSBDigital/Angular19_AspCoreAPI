b66a052b743214c569807b0a6fbc4de5190ed4b5
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-12-04 17:46:25 +0530

    Add nested routes for layout and download form
    Introduced a route with LayoutComponent as the parent, nesting DashboardComponent and DwnldRegFormComponent under it. This enables structured navigation and supports the download registration form within the layout.

--- BSEBApp/src/app/app.routes.ts
+++ BSEBApp/src/app/app.routes.ts
@@ -1,12 +1,21 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DwnldRegFormComponent } from './pages/dwnld-reg-form/dwnld-reg-form.component';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 export const routes: Routes = [
     // { path: 'login', component: LoginComponent },
     // {path: '**', redirectTo: 'login'},
     {path: '',redirectTo: 'login',pathMatch: 'full'},
     {path: 'login',loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) },
-    {path: 'dashboard',loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent) }
+    {path: 'dashboard',loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent) },
+    {path : '', component: LayoutComponent,
+        children: [
+        { path: '', component: DashboardComponent },
+        { path: 'dwnld-reg-form', component: DwnldRegFormComponent },
+        ]
+    }
 
+//    { path: 'dwnld-reg-form', component: DwnldRegFormComponent }
 ];
